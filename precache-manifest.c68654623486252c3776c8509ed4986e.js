09561568f36fc0ec5814d31b3d0bd07f4837a2e4
mukhindev <mukhin.dev@gmail.com>
2020-10-04 20:14:51 +0300

    Updates

---
+++ precache-manifest.c68654623486252c3776c8509ed4986e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260bf01cc432a9bcb4e8c7e2df1ee747",
+    "url": "/jsonplaceholder-with-react-and-redux/index.html"
+  },
+  {
+    "revision": "93012fc0f5838aa92aa5",
+    "url": "/jsonplaceholder-with-react-and-redux/static/css/main.2ce55943.chunk.css"
+  },
+  {
+    "revision": "71b2674b4f4316641f4a",
+    "url": "/jsonplaceholder-with-react-and-redux/static/js/2.0203fdd7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jsonplaceholder-with-react-and-redux/static/js/2.0203fdd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93012fc0f5838aa92aa5",
+    "url": "/jsonplaceholder-with-react-and-redux/static/js/main.b3ce74b3.chunk.js"
+  },
+  {
+    "revision": "ead4b68a468171874eac",
+    "url": "/jsonplaceholder-with-react-and-redux/static/js/runtime-main.a92910f0.js"
+  }
+]);
